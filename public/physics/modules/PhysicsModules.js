e4d1004b2c28fd38b2080405b307ac9d1a583075
Eli <136011323+eliaselliotson@users.noreply.github.com>
2025-08-22 01:02:29 +0000

    Correct some of the shape parameters

--- public/physics/modules/PhysicsModules.js
+++ public/physics/modules/PhysicsModules.js
@@ -14,8 +14,8 @@ export const Modules = {
   Shapes: {
     capsule: (options, x, y, length, radius, angle = 0) => new Capsule({ ...options, position: [x, y], length, radius, angle }),
     circle: (options, x, y, radius, angle = 0) => new Circle({ ...options, position: [x, y], radius, angle }),
-    convex: (options, x, y, vertices, angle = 0) => new Convex({ ...options, position: [x, y], vertices, angle }),
-    heightfield: (options, x, y, heights, elementWidth, angle = 0) => new Heightfield({ ...options, position: [x, y], heights, elementWidth, angle }),
+    convex: (options, x, y, vertices, angle = 0) => new Convex({ ...options, position: [x, y], vertices: vertices.map(e => e.toArray()), angle }),
+    heightfield: (options, x, y, heights, spacing, angle = 0) => new Heightfield({ ...options, position: [x, y], heights, elementWidth: spacing, angle }),
     line: (options, x, y, length, angle = 0) => new Line({ ...options, position: [x, y], length, angle }),
     plane: (options, x, y, angle = 0) => new Plane({ ...options, position: [x, y], angle }),
   },
